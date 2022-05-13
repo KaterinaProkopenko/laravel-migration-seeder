7dff0c1b2ebd0795ef8281e387f97689b2295215
KaterinaProkopenko <ekaprokopenko97@gmail.com>
2022-05-13 17:56:25 +0200

    Aggiornamento (train-card)

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Train;
 use Illuminate\Http\Request;
 
 class TrainController extends Controller
 {
-    //
+    public function index(){
+        $trains = Train::orderBy('orario_di_partenza', 'ASC')->get();
+
+        return view('trains.index', ["trains" => $trains, "train" => new Train()]);
+    }
 }
